a789f763c946c87b3b395c364a029d88af1a3526
Paul Rosero <paul_rosero@yahoo.com>
2019-05-26 19:44:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(sring)
+    @knowledge << d
+  end
+  
+  def method(array)
+    @knowledge
+  en
 end
